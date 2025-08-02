a81876734330438e4bd5a51f069e0b6f97924c74
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2025-08-02 10:38:05 +0200

    Belated happy new year

--- src/test/java/com/tupilabs/human_name_parser/BuilderTest.java
+++ src/test/java/com/tupilabs/human_name_parser/BuilderTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2010-2020 Jason Priem, Bruno P. Kinoshita
+ * Copyright (c) 2010-2025 Jason Priem, Bruno P. Kinoshita
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
